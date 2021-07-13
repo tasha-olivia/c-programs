e235a73a31f1c801f662d498b40271976cc0c83d
tasha-olivia <tashaolivia9@gmail.com>
2021-07-13 14:26:07 +0100

    Sum of two numbers in C

---
+++ sum two numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	// code to sum two numbers
+	int a, b, sum;
+	printf("enter two numbers \n");
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("the sum of the two numbers is %d", sum); 
+	
+	return 0;
+}
