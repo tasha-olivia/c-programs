7512e58e2d73c4e17cb62af09095490c96fa18bd
tasha-olivia <tashaolivia9@gmail.com>
2021-08-18 13:24:29 +0100

    sums first 10 number

---
+++ sum-first-10-numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+// sums numbers from 0 to 10
+int main()
+{
+int i=0, n=0;
+for(i = 0; i<=10; i++){
+printf("%d\n", i);
+n = n + i;
+}
+printf("the sum is %d\n", n);
+
+    return 0;
+}
